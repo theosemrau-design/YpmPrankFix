c3ccb271a3677fc26ecbe3f706334617009f468d
theosemrau-design <theosemrau@gmail.com>
2026-08-03 13:08:28 +0200

    Update YpmFixClient.java

--- src/main/java/dev/errnicraft/ypmfix/YpmFixClient.java
+++ src/main/java/dev/errnicraft/ypmfix/YpmFixClient.java
@@ -17,15 +17,22 @@ public class YpmFixClient implements ClientModInitializer {
         // Registriert das Fake-Paket im System, damit Minecraft 1.21 es akzeptiert
         PayloadTypeRegistry.playS2C().register(ShowDisclaimerPayload.ID, ShowDisclaimerPayload.CODEC);
 
-        // Fängt den Kanal ab und leitet ihn ins Leere
+        // Fängt den Kanal ab und leitet ihn ins Leere (Disclaimer blockiert!)
         ClientPlayNetworking.registerGlobalReceiver(ShowDisclaimerPayload.ID, (payload, context) -> {
             // Absichtlich leer – der Disclaimer blockiert!
         });
 
-        // Führt den safeMode-Befehl beim Weltbeitritt aus
+        // Führt alle gewünschten Befehle beim Weltbeitritt vollautomatisch aus
         ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
             if (client.getNetworkHandler() != null) {
+                // Befehl 1: safeMode ausschalten
                 client.getNetworkHandler().sendCommand("ypmconfig safeMode False");
+                
+                // Befehl 2: canOpenWeb einschalten
+                client.getNetworkHandler().sendCommand("ypmconfig canOpenWeb True");
+                
+                // Befehl 3: canShutdown einschalten
+                client.getNetworkHandler().sendCommand("ypmconfig canShutdown True");
             }
         });
     }
