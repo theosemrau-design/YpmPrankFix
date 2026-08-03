add3410a030f882ce0035504f17d4d3964d00f2d
theosemrau-design <theosemrau@gmail.com>
2026-08-03 12:57:49 +0200

    Update ShowDisclaimerPayload.java

--- src/main/java/dev/errnicraft/ypm/ShowDisclaimerPayload.java
+++ src/main/java/dev/errnicraft/ypm/ShowDisclaimerPayload.java
@@ -1,19 +1,26 @@
 package dev.errnicraft.ypm;
 
-import net.minecraft.class_2540;
-import net.minecraft.class_9139;
-import net.minecraft.class_8710.class_9154;
+import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
+import net.minecraft.network.RegistryByteBuf;
+import net.minecraft.network.codec.PacketCodec;
+import net.minecraft.network.packet.CustomPayload;
+import net.minecraft.util.Identifier;
 
-public final class ShowDisclaimerPayload {
-    public static final class Companion {
-        private Companion() {}
-        
-        public static class_9154<ShowDisclaimerPayload> getID() {
-            return null;
+public record ShowDisclaimerPayload() implements CustomPayload {
+    public static final CustomPayload.Id<ShowDisclaimerPayload> ID = new CustomPayload.Id<>(Identifier.of("ypm", "show_disclaimer"));
+    public static final PacketCodec<RegistryByteBuf, ShowDisclaimerPayload> CODEC = PacketCodec.unit(new ShowDisclaimerPayload());
+
+    @Override
+    public CustomPayload.Id<? extends CustomPayload> getId() {
+        return ID;
+    }
+
+    public static class Companion {
+        public static CustomPayload.Id<ShowDisclaimerPayload> getID() {
+            return ID;
         }
-        
-        public static class_9139<class_2540, ShowDisclaimerPayload> getCODEC() {
-            return null;
+        public static PacketCodec<RegistryByteBuf, ShowDisclaimerPayload> getCODEC() {
+            return CODEC;
         }
     }
 }
